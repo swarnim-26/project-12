02969983235f8df52df8b8921c636d2b81854db0
Swarnim Tiwari <swarnimtiwari2aug2006@gmail.com>
2025-08-07 22:28:28 +0530

    second commit

---
+++ dp10/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    
+    return HttpResponse("Home page")
+
+
+def home(request):
+    return render(request, 'index.html')
